532cd88e648d2f15f24afc33fb22c25c14c86b46
Raihan Mustofa <raihanmustofa.11656@gmail.com>
2025-05-04 23:04:06 +0700

    CORS

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 let corsOptions = {
-    origin: '*', // Lebih permisif untuk development
+    origin: process.env.FRONTEND_URL || '*', // Lebih permisif untuk development
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
